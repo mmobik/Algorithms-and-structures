cf63c9993b10cbf2255afbf48f04c0875e678261
Sergey Oshepkov <hak189145@gmail.com>
2025-11-28 21:00:33 +0500

    feat: Update package version and include package data for algorithms and structures modules

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="Algorithms_and_structures",
-    version="1.0.4",
+    version="1.0.5",
     packages=find_packages(),
     author="Sergey O",
     package_dir={'Algorithms_and_structures': 'Algorithms_and_structures'},
+    include_package_data=True,
+    package_data={
+        'Algorithms_and_structures.Structures': ['*.py'],
+        'Algorithms_and_structures.Algorithms': ['*.py'],
+    },
     description="Data structures and algorithms implementations for learning purposes",
     long_description="This repository was created primarily for training in creating own libraries. Consider this as an experiment.",
     python_requires=">=3.6",
